fa9218260be2e8b52c945c5d135cc3399c9819b4
asad <133575952+trustful331@users.noreply.github.com>
2023-05-20 17:32:13 +0500

    Design updates

--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,8 +1,26 @@
 import Banner from "./Banner";
 import { BiBed } from "react-icons/bi";
+import React, { useEffect, useState } from "react";
 import { TbBuildingEstate, TbBuildingSkyscraper } from "react-icons/tb";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    AOS.init();
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div>
       {/* Banner */}
@@ -33,87 +51,117 @@ export default function Home() {
             <p className="text-[18px] text-[#232728]/70 font-normal">Beds</p>
           </div>
         </div>
-        <div className="container mt-12">
-          <h1 className="text-[86px] font-bold mt-[90px] mb-[80px] text-center">
-            The Comfort of Your Home
-          </h1>
-          <p className="text-[18px] text-black/50 leading-[30px] w-[50vw] mx-auto text-center mb-14">
-            Living in a new city is very challenging for everyone and notably,
-            if you’re a young person it will be quite scary as well. Missing
-            your comforts of home, the warmness, the emotions and it is
-            difficult to adjust. Worry no more because Cozy Stay helps you
-            locate the optimal coliving spaces that add comfort and solace to
-            your home by mirroring the amenities you call your own.
-          </p>
-          <div className="grid grid-cols-3 gap-8">
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
-              </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
+      </div>
+      <div className="relative mt-[200px]">
+        <img
+          src="/assets/images/device.png"
+          className={`w-[24%] absolute transition-all duration-[800ms] ${
+            scroll ? "bottom-0 left-[15%]" : "left-0 -bottom-52"
+          }`}
+        />
+        <img
+          src="/assets/images/device_bg.jpg"
+          className="w-full h-[800px] object-cover"
+        />
+        <img
+          src="/assets/images/device.png"
+          className={`w-[24%] absolute transition-all duration-[800ms] ${
+            scroll ? "bottom-0 right-[15%]" : "right-0 -bottom-52"
+          }  `}
+        />
+      </div>
+      <div className="container mt-12">
+        <h1 className="text-[86px] font-bold mt-[90px] mb-[80px] text-center">
+          The Comfort of Your Home
+        </h1>
+        <p className="text-[18px] text-black/50 leading-[30px] w-[50vw] mx-auto text-center mb-14">
+          Living in a new city is very challenging for everyone and notably, if
+          you’re a young person it will be quite scary as well. Missing your
+          comforts of home, the warmness, the emotions and it is difficult to
+          adjust. Worry no more because Cozy Stay helps you locate the optimal
+          coliving spaces that add comfort and solace to your home by mirroring
+          the amenities you call your own.
+        </p>
+        <div className="grid grid-cols-3 gap-8">
+          <div data-aos="zoom-in-right">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
             </div>
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
-              </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+          <div data-aos="zoom-in-up">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
             </div>
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
-              </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+          <div data-aos="zoom-in-left">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
             </div>
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
-              </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+          <div data-aos="zoom-in-right">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
             </div>
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
-              </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+          <div data-aos="zoom-in-up">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
             </div>
-            <div>
-              <div className="rounded-xl p-3 bg-white shadow-sm">
-                <img
-                  src="assets/images/demo.png"
-                  className="rounded-xl w-full"
-                />
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+          <div data-aos="zoom-in-left">
+            <div className="rounded-xl p-3 bg-white shadow-sm">
+              <img src="assets/images/demo.png" className="rounded-xl w-full" />
+            </div>
+            <h4 className="text-[24px] font-semibold text-center mt-4">
+              Title title title
+            </h4>
+          </div>
+        </div>
+      </div>
+      <div
+        className="relative mt-[200px] cities-bg flex items-end"
+        data-aos="fade-up"
+      >
+        <div className="container">
+          <div className="grid grid-cols-12 gap-5">
+            <div className="col-span-4"></div>
+            <div className="col-span-8">
+              <div className="grid grid-cols-3 gap-5 mb-24 items-center">
+                <div>
+                  <img
+                    src="/assets/images/kochi.png"
+                    className="w-full animate-bounce"
+                  />
+                </div>
+                <div>
+                  <img src="/assets/images/bang.png" className="w-full" />
+                </div>
+                <div>
+                  <img
+                    src="/assets/images/chennai.png"
+                    className="w-full animate-bounce"
+                  />
+                </div>
               </div>
-              <h4 className="text-[24px] font-semibold text-center mt-4">
-                Title title title
-              </h4>
             </div>
           </div>
         </div>
+        {/* <img src="/assets/images/clipper.png" className="w-full object-cover" /> */}
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,8 @@
 import Home from "@/container/Home";
-import React, { useEffect, useState } from 'react';
 export default function HomePage() {
-  const [scroll, setScroll] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 1600) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
   return (
     <>
       <Home />
-      <div className="relative mt-[200px]">
-        <img src="/assets/images/device.png" className={`w-[24%] absolute transition-all duration-[800ms] ${scroll ? "bottom-0 left-[15%]" : "left-0 -bottom-52"}`} />
-        <img src="/assets/images/device_bg.jpg" className="w-full h-[800px] object-cover" />
-        <img src="/assets/images/device.png" className={`w-[24%] absolute transition-all duration-[800ms] ${scroll ? "bottom-0 right-[15%]" : "right-0 -bottom-52"}  `} />
-      </div>
     </>
   );
 }
